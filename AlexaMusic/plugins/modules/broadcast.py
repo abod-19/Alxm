451fa95d5456a82e7777bb6e8816727a02b2fb5f
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-07-21 22:12:33 +0300

    Update broadcast.py

--- AlexaMusic/plugins/modules/broadcast.py
+++ AlexaMusic/plugins/modules/broadcast.py
@@ -69,7 +69,7 @@ async def clean_mode(client, update, users, chats):
 async def braodcast_message(client, message, _):
     if message.from_user.id not in OWNER_ID:
         return await message.reply_text(
-            "» **فقط مالك البوت من يمكنه الاذاعة 🤫."
+            "» **فقط مالك البوت من يمكنه الاذاعة 🤫.**"
         )
     global IS_BROADCASTING
     if message.reply_to_message:
