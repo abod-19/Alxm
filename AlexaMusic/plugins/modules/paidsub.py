043ea83973889e7ef44156e92ff96374c593c1d3
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-07-21 22:20:53 +0300

    Update paidsub.py

--- AlexaMusic/plugins/modules/paidsub.py
+++ AlexaMusic/plugins/modules/paidsub.py
@@ -1,15 +1,3 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-""""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
 import asyncio
 from AlexaMusic import app
 from pyrogram import Client, filters
@@ -20,4 +8,4 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from AlexaMusic.utils.database import get_served_users, get_served_chats
 
 
-OWNER_ID = 6174058850
+OWNER_ID = 5145609515
