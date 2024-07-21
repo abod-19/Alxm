5f8c9a7ebf522db2f2bfa946e50a81c7b08d83cd
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-07-21 21:15:35 +0300

    Update pause.py

--- AlexaMusic/plugins/admins/pause.py
+++ AlexaMusic/plugins/admins/pause.py
@@ -1,15 +1,3 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-""""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
 from pyrogram import filters
 from pyrogram.types import Message
 
@@ -20,13 +8,8 @@ from AlexaMusic.core.call import Alexa
 from AlexaMusic.utils.database import is_music_playing, music_off
 from AlexaMusic.utils.decorators import AdminRightsCheck
 
-# Commands
-PAUSE_COMMAND = get_command("PAUSE_COMMAND")
 
-
-@app.on_message(
- filters.command(PAUSE_COMMAND,"") 
-    & ~BANNED_USERS)
+@app.on_message(filters.command(["pause", "cpause","ايقاف مؤقت","إيقاف مؤقت","وقف", "توقف"],"") & ~BANNED_USERS)
 @AdminRightsCheck
 async def pause_admin(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
