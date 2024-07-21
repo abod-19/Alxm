bfc2a790886bdaa2d62347b24f06ed140dc3f948
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-07-21 20:53:14 +0300

    Update userbot.py

--- AlexaMusic/core/userbot.py
+++ AlexaMusic/core/userbot.py
@@ -1,15 +1,3 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-""""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
 import sys
 
 from pyrogram import Client
@@ -65,7 +53,7 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("ah07v")
+                await self.one.join_chat("EF_19")
             except:
                 pass
             assistants.append(1)
