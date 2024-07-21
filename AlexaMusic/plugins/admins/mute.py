3d8cbaa5ba186e46c72d229dbf323937d69c5de4
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-07-21 21:14:02 +0300

    Update mute.py

--- AlexaMusic/plugins/admins/mute.py
+++ AlexaMusic/plugins/admins/mute.py
@@ -1,32 +1,13 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-""""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
 from pyrogram import filters
 from pyrogram.types import Message
 
 from config import BANNED_USERS
-from strings import get_command
 from AlexaMusic import app
 from AlexaMusic.core.call import Alexa
 from AlexaMusic.utils.database import is_muted, mute_on
 from AlexaMusic.utils.decorators import AdminRightsCheck
 
-# Commands
-MUTE_COMMAND = get_command("MUTE_COMMAND")
-
-
-@app.on_message(
- filters.command(MUTE_COMMAND,"")
-    & ~BANNED_USERS)
+@app.on_message(filters.command(["pause", "cpause","ايقاف مؤقت","إيقاف مؤقت","وقف", "توقف"],"") & ~BANNED_USERS)
 @AdminRightsCheck
 async def mute_admin(cli, message: Message, _, chat_id):
     if not len(message.command) == 1 or message.reply_to_message:
