3b90c85fa573b75c19e3afad94597d72aea290c7
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-07-22 18:23:08 +0300

    Update playmode.py

--- AlexaMusic/plugins/play/playmode.py
+++ AlexaMusic/plugins/play/playmode.py
@@ -1,15 +1,3 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-""""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
 
@@ -20,11 +8,8 @@ from AlexaMusic.utils.database import get_playmode, get_playtype, is_nonadmin_ch
 from AlexaMusic.utils.decorators import language
 from AlexaMusic.utils.inline.settings import playmode_users_markup
 
-### Commands
-PLAYMODE_COMMAND = get_command("PLAYMODE_COMMAND")
 
-
-@app.on_message(filters.command(PLAYMODE_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["/playmode", "/mode", "وضع التشغيل"],"") & filters.group & ~BANNED_USERS)
 @language
 async def playmode_(client, message: Message, _):
     playmode = await get_playmode(message.chat.id)
