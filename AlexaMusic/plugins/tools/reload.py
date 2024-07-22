ef6449c2ef4a17e20d5d56748bcbbd2b8d1bd1a0
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-07-22 19:42:52 +0300

    Update reload.py

--- AlexaMusic/plugins/tools/reload.py
+++ AlexaMusic/plugins/tools/reload.py
@@ -1,14 +1,3 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-""""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
 import asyncio
 
 from pyrogram import filters
@@ -29,7 +18,7 @@ RELOAD_COMMAND = get_command("RELOAD_COMMAND")
 RESTART_COMMAND = get_command("RESTART_COMMAND")
 
 
-@app.on_message(filters.command(RELOAD_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["admincache", "/reload","reload", "refresh","ريلود"],"") & filters.group & ~BANNED_USERS)
 @language
 async def reload_admin_cache(client, message: Message, _):
     try:
@@ -50,7 +39,7 @@ async def reload_admin_cache(client, message: Message, _):
         )
 
 
-@app.on_message(filters.command(RESTART_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["reboot", "/reboot","ريبوت"],"") & filters.group & ~BANNED_USERS)
 @AdminActual
 async def restartbot(client, message: Message, _):
     mystic = await message.reply_text(
