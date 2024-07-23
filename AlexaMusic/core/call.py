63d3706f7155ab19efddf3bf4e792623edaf4a88
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-07-23 12:19:15 +0300

    Update call.py

--- AlexaMusic/core/call.py
+++ AlexaMusic/core/call.py
@@ -615,7 +615,7 @@ class Call(PyTgCalls):
         return str(round(sum(pings) / len(pings), 3))
 
     async def start(self):
-        LOGGER(__name__).info("Starting PyTgCalls Client\n")
+        LOGGER(__name__).info("\n بدء عملية PyTgCalls لتشغيل الحساب المساعد\n")
         if config.STRING1:
             await self.one.start()
         if config.STRING2:
