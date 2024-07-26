7a9d94d239cc1707a4eb1332407a813bec81f318
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-07-26 23:22:11 +0300

    Update mute.py

--- AlexaMusic/plugins/admins/mute.py
+++ AlexaMusic/plugins/admins/mute.py
@@ -10,12 +10,9 @@ from AlexaMusic.utils.decorators import AdminRightsCheck
 @app.on_message(filters.command(["mute", "cmute", "اسكت", "/mute", "/cmute"],"") & ~BANNED_USERS)
 @AdminRightsCheck
 async def mute_admin(cli, message: Message, _, chat_id):
-    if not len(message.command) == 1 or message.reply_to_message:
-        return await message.reply_text(_["general_2"])
     if await is_muted(chat_id):
         return await message.reply_text(_["admin_5"], disable_web_page_preview=True)
     await mute_on(chat_id)
-    user_mention = message.from_user.mention if message.from_user else "المشـرف"
     await Alexa.mute_stream(chat_id)
     await message.reply_text(
         _["admin_6"].format(message.from_user.mention), disable_web_page_preview=True
