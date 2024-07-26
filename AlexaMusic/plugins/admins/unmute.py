8ded2506d21616e2660345ecb3bc9d9a2b119f1d
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-07-26 23:40:40 +0300

    Update unmute.py

--- AlexaMusic/plugins/admins/unmute.py
+++ AlexaMusic/plugins/admins/unmute.py
@@ -10,16 +10,12 @@ from AlexaMusic.utils.decorators import AdminRightsCheck
 
 
 @app.on_message(
- filters.command(["unmute", "cunmute", "اتكلم", "/unmute", "/cunmute"],"")
-    & ~BANNED_USERS)
+ filters.command(["unmute", "cunmute", "اتكلم", "/unmute", "/cunmute"],"") & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def unmute_admin(Client, message: Message, _, chat_id):
-    if not len(message.command) == 1 or message.reply_to_message:
-        return await message.reply_text(_["general_2"])
     if not await is_muted(chat_id):
         return await message.reply_text(_["admin_7"], disable_web_page_preview=True)
     await mute_off(chat_id)
-    user_mention = message.from_user.mention if message.from_user else "المشـرف"
     await Alexa.unmute_stream(chat_id)
     await message.reply_text(
         _["admin_8"].format(message.from_user.mention), disable_web_page_preview=True
