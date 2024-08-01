68f84e8633198263c066b11194d3c89a2630ac8d
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-08-01 11:34:45 +0300

    Update botwelcomlog.py

--- AlexaMusic/plugins/modules/botwelcomlog.py
+++ AlexaMusic/plugins/modules/botwelcomlog.py
@@ -1,9 +1,3 @@
-# This code is written by (C) TheTeamAlexa bot will send message to log group when someone add
-# this bot to new group make sure to star all projects
-# Copyright (C) 2021-2024 by Alexa_Help@ Github, < TheTeamAlexa >.
-# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
-# All rights reserved. © Alisha © Alexa © Yukki
-
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from AlexaMusic import app
@@ -19,9 +13,14 @@ async def lul_message(chat_id: int, message: str):
 async def on_new_chat_members(client: Client, message: Message):
     if (await client.get_me()).id in [user.id for user in message.new_chat_members]:
         added_by = message.from_user.first_name if message.from_user else "ᴜɴᴋɴᴏᴡɴ ᴜsᴇʀ"
+        
         matlabi_jhanto = message.chat.title
-        served_chats = len(await get_served_chats())
+        served_chats = len(await get_served_chats())g
         chat_id = message.chat.id
+
+        chat = await client.get_chat(int(chat_id))
+        cont = chat.members_count
+        
         if message.chat.username:
             chatusername = f"@{message.chat.username}"
         else:
